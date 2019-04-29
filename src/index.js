6920cc3164f60cc9d62afa37299b6e68fcd1ab6e
Tai Lai <ctlai95@gmail.com>
2019-04-29 16:29:11 -0600

    Add for loop to board render function

--- src/index.js
+++ src/index.js
@@ -16,29 +16,23 @@ class Board extends React.Component {
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                key={i}
             />
         );
     }
 
     render() {
+        const items = [];
+        for (let i = 0; i < 3; i++) {
+            const squares = [];
+            for (let j = i * 3; j < i * 3 + 3; j++) {
+                squares.push(this.renderSquare(j));
+            }
+            items.push(<div className="board-row" key={i} >{squares}</div>);
+        }
+
         return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+            <div>{items}</div >
         );
     }
 }
