9481b5ddc978e4ee5ad60a6ddb77872fa73a1631
Tai Lai <ctlai95@gmail.com>
2019-05-01 08:19:05 -0600

    Updates

---
+++ precache-manifest.3486d95f427ce179734faacf0053d461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2f00da605c3145998d5ba42901588e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "47eafff28530271b667c",
+    "url": "/react-tutorial/static/css/main.3502e27f.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-tutorial/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "47eafff28530271b667c",
+    "url": "/react-tutorial/static/js/main.fc1bffd3.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
